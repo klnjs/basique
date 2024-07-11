e64f05b82c87a934417b317b2619133a675794ed
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-07-12 00:26:34 +0200

    🎨 Just some formatting (#78)

--- packages/headers/permissions-policy/test/pp.spec.ts
+++ packages/headers/permissions-policy/test/pp.spec.ts
@@ -4,9 +4,7 @@ import PP from '../src/pp'
 test('parse should succeed with valid permissions-policy', () => {
 	expect(() => PP.parse('camera=*')).not.toThrow()
 	expect(() => PP.parse('camera=()')).not.toThrow()
-	expect(() =>
-		PP.parse('camera=(self "https://trusted.site.com")')
-	).not.toThrow()
+	expect(() => PP.parse('camera=(self "https://trusted.com")')).not.toThrow()
 })
 
 test('parse should throw with invalid permissions-policy', () => {
