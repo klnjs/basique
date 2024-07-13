0446f7132030b0d41562be169d535dc754778763
Rune Klein <runeklein@outlook.com>
2024-07-13 22:34:33 +0200

    ✨ Implement reporting-endpoints header

---
+++ packages/headers/reporting-endpoints/src/re.ts
@@ -0,0 +1,27 @@
+export type ReportingEndpoints = Record<string, `http://${string}` | `https://${string}`>
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export default class RE {
+	static parse(text: string): ReportingEndpoints {
+		return text.split(',').reduce((acc: ReportingEndpoints, entry) => {
+			const [endpoint = '', url = ''] = entry.trim().split('=')
+
+			if (!/^(?:http:\/\/.*|https:\/\/.*)$/.test(url)) {
+				throw new SyntaxError(
+					`ReportingEndpoints.parse: invalid url "${url}" for endpoint "${endpoint}"`
+				)
+			}
+
+			return {
+				...acc,
+				[endpoint]: url
+			} as ReportingEndpoints
+		}, {})
+	}
+
+	static stringify(endpoints: ReportingEndpoints): string {
+		return Object.entries(endpoints)
+			.map(([name, url]) => `${name}=${url}`)
+			.join(', ')
+	}
+}
