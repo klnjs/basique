5c67b61fdc985ec114d0621bc4817fa2cf354b9e
Rune Klein <runeklein@outlook.com>
2024-07-13 22:55:56 +0200

    🐛 prettier

--- packages/headers/reporting-endpoints/src/re.ts
+++ packages/headers/reporting-endpoints/src/re.ts
@@ -1,6 +1,4 @@
-export type ReportingEndpoints = Record<
-	string, `https://${string}`
->
+export type ReportingEndpoints = Record<string, `https://${string}`>
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class RE {
