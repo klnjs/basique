b06046a7379e9fde51404c010b805b6db74c3831
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-07-13 23:03:55 +0200

    🚚 Remove temp folder in favour of __experiemental (#90)

--- packages/react/__experimental/calendar-field/CalendarFieldInput.tsx
+++ packages/react/__experimental/calendar-field/CalendarFieldInput.tsx
@@ -1,5 +1,5 @@
 import { useCallback, type PointerEvent } from 'react'
-import { poly, forwardRef, asDataProp, type CoreProps } from '../core'
+import { poly, forwardRef, asDataProp, type CoreProps } from '@klnjs/core'
 import { useCalendarFieldContext } from './CalendarFieldContext'
 
 export type CalendarFieldInputProps = CoreProps<'div'>
