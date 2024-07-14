6e49483099052683fe739579b4026f1b7e731741
Rune Klein <runeklein@outlook.com>
2024-07-15 00:55:38 +0200

    🐛 Fix eslint

--- packages/tools/basique/src/prepublish.ts
+++ packages/tools/basique/src/prepublish.ts
@@ -1,7 +1,5 @@
 #!/usr/bin/env bun
 
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
