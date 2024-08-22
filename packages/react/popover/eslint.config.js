5575ed3e6f457528b45a33cce60e69b2e2d9fd95
Rune Klein <runeklein@outlook.com>
2024-08-22 21:29:01 +0200

    ⬆️ Upgrade all dependencies

--- packages/react/popover/eslint.config.js
+++ packages/react/popover/eslint.config.js
@@ -1,10 +1,6 @@
-import { core, react, typescript } from '@klnjs/eslint-config'
+import core from '@klnjs/eslint-config/core'
+import react from '@klnjs/eslint-config/react'
+import ignores from '@klnjs/eslint-config/ignores'
+import typescript from '@klnjs/eslint-config/typescript'
 
-export default [
-	core,
-	react,
-	typescript,
-	{
-		ignores: ['dist']
-	}
-]
+export default [core, react, typescript, ignores]
