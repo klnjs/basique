149a3f18388609dc8d32d27f408c12db5bde6b2e
Rune Klein <runeklein@outlook.com>
2024-09-04 00:05:38 +0200

    💄 Fix up some of the styles

--- apps/starlight/src/content/docs/components/paginator/paginator.tsx
+++ apps/starlight/src/content/docs/components/paginator/paginator.tsx
@@ -8,32 +8,24 @@ import {
 import classes from './paginator.module.css'
 
 export default () => (
-	<div
-		style={{
-			display: 'flex',
-			flexDirection: 'column',
-			alignItems: 'center'
-		}}
-	>
-		<Paginator pages={10} defaultPage={5} className={classes.paginator}>
-			<PaginatorButton action="dec" className={classes.button} />
-			<PaginatorPages>
-				{({ type, page }) =>
-					type === 'ellipsis' ? (
-						<PaginatorEllipsis
-							key={page}
-							className={classes.ellipsis}
-						/>
-					) : (
-						<PaginatorPage
-							key={page}
-							page={page}
-							className={classes.page}
-						/>
-					)
-				}
-			</PaginatorPages>
-			<PaginatorButton action="inc" className={classes.button} />
-		</Paginator>
-	</div>
+	<Paginator pages={10} defaultPage={5} className={classes.paginator}>
+		<PaginatorButton action="dec" className={classes.button} />
+		<PaginatorPages>
+			{({ type, page }) =>
+				type === 'ellipsis' ? (
+					<PaginatorEllipsis
+						key={page}
+						className={classes.ellipsis}
+					/>
+				) : (
+					<PaginatorPage
+						key={page}
+						page={page}
+						className={classes.page}
+					/>
+				)
+			}
+		</PaginatorPages>
+		<PaginatorButton action="inc" className={classes.button} />
+	</Paginator>
 )
