fb05b9cc93c70f3d3b417684d66974d5e6e5f023
Rune Klein <runeklein@outlook.com>
2024-09-03 23:05:56 +0200

    🐛 Fixes

--- packages/tools/cli/index.ts
+++ packages/tools/cli/index.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env bun
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { run as build } from './src/build'
 import { run as prepublish } from './src/prepublish'
 
-yargs(hideBin(process.argv))
+await yargs(hideBin(process.argv))
 	.command('build', 'build a package', () => {}, build)
 	.command(
 		'prepublish',
