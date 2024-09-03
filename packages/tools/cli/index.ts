0b82bcd63cad21f1453ed13dc3e6c0ac437d8eaa
Rune Klein <runeklein@outlook.com>
2024-09-03 22:29:44 +0200

    🐛 fix args

--- packages/tools/cli/index.ts
+++ packages/tools/cli/index.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env bun
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { run as build } from './src/build'
 import { run as prepublish } from './src/prepublish'
 
-await yargs(hideBin(process.argv))
+yargs(hideBin(process.argv))
 	.command('build', 'build a package', () => {}, build)
 	.command(
 		'prepublish',
