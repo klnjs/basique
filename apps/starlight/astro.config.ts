63b2acb560556bc884b4591f8bc51283f49c6d10
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-09-04 00:44:58 +0200

    ✨ Move docs to starlight (#107)

---
+++ apps/starlight/astro.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import starlight from '@astrojs/starlight'
+
+const mdxs = new Bun.Glob('components/**/*.mdx').scan('src/content/docs')
+const components = await Array.fromAsync(mdxs)
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://klnjs.github.io',
+	base: 'basique',
+	integrations: [
+		react(),
+		starlight({
+			title: 'Basique',
+			logo: {
+				src: './src/assets/logo-glyph.svg'
+			},
+			social: {
+				github: 'https://github.com/klnjs/basique'
+			},
+			sidebar: [
+				{ label: 'Start', autogenerate: { directory: 'start' } },
+				{
+					label: 'Components',
+					items: components
+						.map((file) => file.replace(/\.[^/.]+$/, ''))
+						.toSorted()
+				}
+			],
+			components: {
+				PageTitle: './src/library/Title.astro'
+			}
+		})
+	]
+})
--- packages/react/paginator/src/PaginatorPages.tsx
+++ packages/react/paginator/src/PaginatorPages.tsx
@@ -1,46 +1,37 @@
 import { useMemo, type ReactNode } from 'react'
 import { clamp } from '@klnjs/math'
-import { poly, forwardRef, type CoreProps } from '@klnjs/react-core'
 import { usePaginatorContext } from './PaginatorContext'
 import { createPaginatorItems, type PaginatorItem } from './usePaginatorItems'
 
-export type PaginatorPagesProps = CoreProps<
-	'div',
-	{
-		boundary?: number
-		siblings?: number
-		children: (props: PaginatorItem, index: number) => ReactNode
-	}
->
+export type PaginatorPagesProps = {
+	boundary?: number
+	siblings?: number
+	children: (props: PaginatorItem, index: number) => ReactNode
+}
 
-export const PaginatorPages = forwardRef<'div', PaginatorPagesProps>(
-	(
-		{ siblings = Infinity, boundary = 1, children, ...otherProps },
-		forwardedRef
-	) => {
-		const { page, pageStart, pageEnd } = usePaginatorContext()
+export const PaginatorPages = ({
+	siblings = Infinity,
+	boundary = 1,
+	children
+}: PaginatorPagesProps) => {
+	const { page, pageStart, pageEnd } = usePaginatorContext()
 
-		const items = useMemo((): PaginatorItem[] => {
-			const offset = boundary - 1
-			const lower = pageStart + offset
-			const upper = pageEnd - offset
-			const min = lower + 2
-			const max = upper - 2
-			const span = Math.min(max - min, siblings * 2)
-			const start = clamp(page - siblings, min, max - span)
-			const end = clamp(page + siblings, min + span, max)
+	const items = useMemo((): PaginatorItem[] => {
+		const offset = boundary - 1
+		const lower = pageStart + offset
+		const upper = pageEnd - offset
+		const min = lower + 2
+		const max = upper - 2
+		const span = Math.min(max - min, siblings * 2)
+		const start = clamp(page - siblings, min, max - span)
+		const end = clamp(page + siblings, min + span, max)
 
-			return createPaginatorItems(
-				[pageStart, lower],
-				[start, end],
-				[upper, pageEnd]
-			)
-		}, [boundary, siblings, page, pageStart, pageEnd])
-
-		return (
-			<poly.div ref={forwardedRef} {...otherProps}>
-				{items.map(children)}
-			</poly.div>
+		return createPaginatorItems(
+			[pageStart, lower],
+			[start, end],
+			[upper, pageEnd]
 		)
-	}
-)
+	}, [boundary, siblings, page, pageStart, pageEnd])
+
+	return items.map(children)
+}
