02eb8d93aac70f2fc6db84a5aba07c899cf6565d
Rune Klein <runeklein@outlook.com>
2024-09-03 21:47:16 +0200

    🔥 Remove docs

--- apps/docs/src/docs/02-components/spinner/spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner, SpinnerTrack, SpinnerThumb } from '@klnjs/react-spinner'
-import classes from './spinner.module.css'
-
-export default () => (
-	<Spinner className={classes.spinner}>
-		<SpinnerTrack className={classes.track} />
-		<SpinnerThumb className={classes.thumb} />
-	</Spinner>
-)
