26839847fd3f07e82760e18ec152cedc99871df3
Rune Klein <runeklein@outlook.com>
2024-09-21 21:54:23 +0200

    💚 Make docs and build work

--- packages/internal/cli/src/build.ts
+++ packages/internal/cli/src/build.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env bun
 
-import { $ } from 'bun'
-import { join } from 'path'
-import { mkdir, cp, rm } from 'node:fs/promises'
+import { $, type JavaScriptLoader, type TSConfig } from 'bun'
+import p from 'path'
+import fs from 'node:fs/promises'
+import ts from 'typescript'
 
 export type BuildOptions = {
 	react?: boolean
@@ -12,22 +13,56 @@ export type BuildOptions = {
 
 export const run = async (options: BuildOptions) => {
 	const cwd = process.cwd()
-	const dist = join(cwd, 'dist')
+	const dist = p.join(cwd, 'dist')
 
-	await mkdir(dist, { recursive: true })
+	await fs.mkdir(dist, { recursive: true })
 
 	if (options.svelte) {
-		const temp = join(cwd, 'temp')
-
-		await mkdir(temp, { recursive: true })
-		await cp(join(cwd, 'src'), join(temp, 'src'), { recursive: true })
-		await cp(join(cwd, 'index.ts'), join(temp, 'index.ts'), {
-			recursive: true
-		})
-		await $`svelte-package -i ./temp`
-		await rm(temp, { recursive: true, force: true })
-		await rm('.svelte-kit', { recursive: true, force: true })
+		await svelte(cwd, dist)
 	} else {
-		await $`tsc --noEmit false --declaration --composite false --removeComments --allowImportingTsExtensions false`
+		await react(cwd, dist)
 	}
+}
+
+const react = async (root: string, dist: string) => {
+	const tsconfig = Bun.resolveSync('./tsconfig.json', root)
+	const raw = ts.readConfigFile(tsconfig, ts.sys.readFile)
+	const parsed = ts.parseJsonConfigFileContent(raw.config, ts.sys, root)
+
+	await $`tsc --noEmit false --noCheck --declaration --emitDeclarationOnly --composite false --incremental false`
+	await Promise.all(
+		parsed.fileNames.map(async (path) => {
+			const transpiler = new Bun.Transpiler({
+				loader: p.extname(path) as JavaScriptLoader,
+				minifyWhitespace: false,
+				tsconfig: raw.config as TSConfig
+			})
+
+			const out = p.join(
+				dist,
+				path.replace(root, '').replace(/\.tsx?$/g, '.js')
+			)
+
+			const file = Bun.file(path)
+			const content = await file.text()
+			const result = await transpiler.transform(content)
+
+			await Bun.write(out, result)
+		})
+	)
+}
+
+const svelte = async (root: string, dist: string) => {
+	const path = p.join(root, '.svelte-package')
+
+	await fs.mkdir(path, { recursive: true })
+	await fs.cp(p.join(root, 'src'), p.join(path, 'src'), {
+		recursive: true
+	})
+	await fs.cp(p.join(root, 'index.ts'), p.join(path, 'index.ts'), {
+		recursive: true
+	})
+	await $`svelte-package -i ./temp -o ${dist}`
+	await fs.rm('.svelte-kit', { recursive: true, force: true })
+	await fs.rm('.svelte-package', { recursive: true, force: true })
 }
--- packages/react/icons/src/WindowMinimize.ts
+++
@@ -1,7 +0,0 @@
-import { createIcon } from '@klnjs/react-icon'
-
-export default createIcon({
-	title: 'WindowMinimize',
-	path: 'M20,14H4V10H20',
-	viewBox: '0 0 24 24'
-})
