e607e2eeb40e53f7905140a48c8b339c2a63a48e
Rune Klein <runeklein@outlook.com>
2024-09-26 23:39:35 +0200

    🐛 Fix starlight

--- apps/starlight/eslint.config.mjs
+++ apps/starlight/eslint.config.mjs
@@ -1,6 +1,7 @@
 import core from '@klnjs/eslint-config/core'
 import typescript from '@klnjs/eslint-config/typescript'
 import react from '@klnjs/eslint-config/react'
+import svelte from '@klnjs/eslint-config/svelte'
 import ignores from '@klnjs/eslint-config/ignores'
 
-export default [core, typescript, react, ignores]
+export default [core, typescript, react, svelte, ignores]
--- apps/starlight/src/content/docs/components/avatar/avatar.svelte
+++ apps/starlight/src/content/docs/components/avatar/avatar.svelte
@@ -4,7 +4,7 @@
 	import image from './svelte.svg'
 </script>
 
-<Avatar class={classes.avatar} onStatusChange={(status) => console.log(status)}>
+<Avatar class={classes.avatar}>
 	<AvatarImage src={image.src} alt="alt" class={classes.image}  />
 	<AvatarFallback class={classes.fallback}>RK</AvatarFallback>
 </Avatar>
