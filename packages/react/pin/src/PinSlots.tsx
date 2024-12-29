1a0df9bdf79716ce517d63045d858481c0d21312
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-12-29 22:35:04 +0100

    docs: pin docs update (#141)

--- packages/react/pin/src/PinSlots.tsx
+++ packages/react/pin/src/PinSlots.tsx
@@ -1,19 +1,19 @@
 import { useMemo, type ReactNode } from 'react'
 import { usePinContext } from './PinContext'
 
-export type PinSlotsItem = {
+export type PinSlotItem = {
 	slot: number
 	value: string
 }
 
 export type PinSlotsProps = {
-	children: (type: PinSlotsItem, index: number) => ReactNode
+	children: (item: PinSlotItem, index: number) => ReactNode
 }
 
 export const PinSlots = ({ children }: PinSlotsProps) => {
 	const { pin, length } = usePinContext()
 
-	const slots: PinSlotsItem[] = useMemo(
+	const slots: PinSlotItem[] = useMemo(
 		() =>
 			Array.from({ length }, (_x, slot) => ({
 				slot: slot + 1,
