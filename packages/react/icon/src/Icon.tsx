cf99e4287c4c6bd923828f79434d049a02d6cca9
KleinMann <47398070+rnkln@users.noreply.github.com>
2025-04-11 10:12:38 +0200

    feat: simply icon implementation (#150)

--- packages/react/icon/src/Icon.tsx
+++ packages/react/icon/src/Icon.tsx
@@ -3,25 +3,10 @@ import type { CoreProps } from '@klnjs/react-core'
 import { IconTitle } from './IconTitle'
 import { IconDescription } from './IconDescription'
 import { IconPath } from './IconPath'
-import { IconProvider } from './IconContext'
-import { useIcon } from './useIcon'
 
 export type IconProps = CoreProps<'svg'>
 
-export const Icon = (props: IconProps) => {
-	const icon = useIcon()
-
-	return (
-		<IconProvider value={icon}>
-			<svg
-				fill="currentColor"
-				aria-labelledby={icon.labelId}
-				aria-describedby={icon.descriptionId}
-				{...props}
-			/>
-		</IconProvider>
-	)
-}
+export const Icon = (props: IconProps) => <svg fill="currentColor" {...props} />
 
 export const createIcon = ({
 	title,
--- packages/react/icon/src/IconDescription.tsx
+++ packages/react/icon/src/IconDescription.tsx
@@ -1,14 +1,7 @@
-import { useId, type CoreProps } from '@klnjs/react-core'
-import { useIconContext } from './IconContext'
+import type { CoreProps } from '@klnjs/react-core'
 
 export type IconDescriptionProps = CoreProps<'desc'>
 
-export const IconDescription = ({
-	id: idProp,
-	...otherProps
-}: IconDescriptionProps) => {
-	const { setDescriptionId } = useIconContext()
-	const id = useId(idProp, setDescriptionId)
-
-	return <desc id={id} {...otherProps} />
-}
+export const IconDescription = (props: IconDescriptionProps) => (
+	<desc {...props} />
+)
